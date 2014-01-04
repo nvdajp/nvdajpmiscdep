8ca6139bccfa7273301873cb4ac383ed4e34a2b3
Takuya Nishimoto <nishimotz@gmail.com>
2014-01-04 15:11:11 +0900

    fixed regarding outpos

--- source/synthDrivers/jtalk/translator2.py
+++ source/synthDrivers/jtalk/translator2.py
@@ -1,7 +1,7 @@
 # coding: UTF-8
 #translator2.py (Japanese Braille translator Phase 2)
 #A part of NonVisual Desktop Access (NVDA)
-#Copyright (C) 2012-2013 Takuya Nishimoto (NVDA Japanese Team)
+#Copyright (C) 2012-2014 Takuya Nishimoto (NVDA Japanese Team)
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
 
@@ -942,8 +942,8 @@ def japaneseToUnicodeBraille(text, logwrite=_logwrite):
 def makeOutPos(inPos, inlen, outlen):
 	# make outPos
 	outPos = [-1] * inlen
-	for p in range(outlen):
-		if inPos[p] < len(outPos) and (outPos[ inPos[p] ] == -1 or inPos[p] == 0):
+	for p in xrange(outlen-1, -1, -1):
+		if inPos[p] < len(outPos):
 			outPos[ inPos[p] ] = p
 	# fill skipped outPos
 	prev = 0
