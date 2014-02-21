d45fe8fc66159555b8136c9f04c9b3a1fea65245
Takuya Nishimoto <nishimotz@gmail.com>
2014-02-21 14:39:32 +0900

    fixed script

--- jptools/userdicBuilder.cmd
+++ jptools/userdicBuilder.cmd
@@ -1,2 +1,2 @@
-..\include\jtalk\\libopenjtalk\mecab\src\mecab-dict-index.exe -d..\source\synthDrivers\jtalk\dic -u jtusr.dic -f utf-8 -t utf-8 jtusr.csv
+..\include\jtalk\\libopenjtalk\mecab\src\mecab-dict-index.exe -d ..\source\synthDrivers\jtalk\dic -u jtusr.dic -f utf-8 -t utf-8 jtusr.csv
 
