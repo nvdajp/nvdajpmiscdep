3241e55a7777e8425d9a0194e1a633faefb4b5f9
Takuya Nishimoto <nishimotz@gmail.com>
2015-12-13 19:15:53 +0900

    fixes nvdajp/nvdajpmiscdep#13

--- jptools/userdicBuilder.cmd
+++ jptools/userdicBuilder.cmd
@@ -1,2 +1,2 @@
-@rem ..\include\jtalk\\libopenjtalk\mecab\src\mecab-dict-index.exe -d ..\source\synthDrivers\jtalk\dic -u jtusr.dic -f utf-8 -t utf-8 jtusr.csv
+jtusrdic\mecab-dict-index.exe -d ..\source\synthDrivers\jtalk\dic -u jtusr.dic -f utf-8 -t utf-8 jtusr.csv
 
