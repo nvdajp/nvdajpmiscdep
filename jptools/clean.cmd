1f586435b52c069b93d1815f25b8f329ffacf181
Takuya Nishimoto <nishimotz@gmail.com>
2017-06-17 15:34:04 +0900

    clean.cmd should use vcvars32.bat

--- jptools/clean.cmd
+++ jptools/clean.cmd
@@ -1,3 +1,10 @@
+if exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat" goto x64
+call "C:\Program Files\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
+goto done
+:x64
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
+:done
+
 del __*.txt
 del *.pyc
 del jtusr.dic
