21089c277cc231574b4c5fa8ee4481cbd2a584fa
Takuya Nishimoto <nishimotz@gmail.com>
2017-10-10 23:15:09 +0900

    handle errors in build-and-test

--- jptools/build-and-test.cmd
+++ jptools/build-and-test.cmd
@@ -1,9 +1,17 @@
 call ..\include\python-jtalk\vcsetup.cmd
 cd ..\include\jtalk
 call all-clean.cmd
+@if not "%ERRORLEVEL%"=="0" goto onerror
 call all-build.cmd
+@if not "%ERRORLEVEL%"=="0" goto onerror
 call all-install.cmd
+@if not "%ERRORLEVEL%"=="0" goto onerror
 cd ..\python-jtalk
 call clean.cmd
 cd ..\..\jptools
 call test.cmd
+
+exit /b 0
+
+:onerror
+exit /b -1
