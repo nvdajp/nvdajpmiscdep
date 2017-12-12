bf45d1ebbc1318fff3a20ccd854c9fdf023716e7
Takuya Nishimoto <nishimotz@gmail.com>
2017-12-12 23:47:24 +0900

    nvdajp/nvdajp#66

--- source/synthDrivers/jtalk/translator2.py
+++ source/synthDrivers/jtalk/translator2.py
@@ -594,7 +594,7 @@ def should_separate(prev2_mo, prev_mo, mo, next_mo, nabcc=False, logwrite=_logwr
 	# 接頭語・接尾語・造語要素であっても、意味の理解を助ける場合には、
 	# 発音上の切れ目を考慮して区切って書いてよい。
 	if prev_mo.hinshi1 == '接頭詞' and \
-		prev_mo.hyouki not in ('お', 'ご', '旧', '後', '副', '大', '再') and \
+		prev_mo.hyouki in ('貴', '前', '当', '反', '非', '新', '要', '肝', '逆') and \
 		mo.hinshi1 == '名詞' and \
 		mo.hinshi2 != '数':
 		return True
