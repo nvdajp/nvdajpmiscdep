b167e7aa7734edf5f9a2509c083f3d847add1cb4
Takuya Nishimoto <nishimotz@gmail.com>
2019-07-21 17:29:10 +0900

    py3/txt2tags

--- jptools/make_html.cmd
+++ jptools/make_html.cmd
@@ -3,5 +3,6 @@ for /F "usebackq" %%t in (`python -c "from __future__ import print_function; imp
 if not "%PYTHONVERSION%"=="2" goto notpythontwo
 python txt2tags.py -t xhtml --toc __jpBrailleHarness.t2t
 python -c "import sys;lines = [line.strip() for line in sys.stdin.readlines()];import re;p = re.compile(r'<a href=\x22(mailto|http):[^>]+>([^<]+)</a>');print '\n'.join(map(lambda l:p.sub(r'\2', l), lines))" < __jpBrailleHarness.xhtml > jpBrailleHarness.xhtml
+exit /b 0
 :notpythontwo
 python py3/txt2tags.py -t xhtml --toc jpBrailleHarness.xhtml
